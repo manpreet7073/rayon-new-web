747d45cad28aa2772e774ed15367832f6c768e78
tom <tom@dimoapp.com>
2025-03-26 16:26:50 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import SitemapPlugin from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    SitemapPlugin({
+      baseUrl: 'https://rayonweb.com',  // Replace with your actual domain
+      sitemapPath: 'sitemap.xml',          // The location where the sitemap will be generated
+    }),
+  ],
   server: {
     port: 3000,
   },
